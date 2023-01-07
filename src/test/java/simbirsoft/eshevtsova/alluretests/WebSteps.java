3b5300f323135135e19b5b78c9549eecdc07c02f
eshevtsova <eshevtsova1987@yandex.ru>
2023-01-07 17:25:17 +0400

    edit  WebSteps.java

--- src/test/java/simbirsoft/eshevtsova/alluretests/WebSteps.java
+++ src/test/java/simbirsoft/eshevtsova/alluretests/WebSteps.java
@@ -5,19 +5,24 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
+
 public class WebSteps {
+
     @Step("Открыть главную страницу GitHub")
     public void openMainPage() {
         open("https://github.com/");
     }
+
     @Step("В поле Search GitHub ввести критерий поиска {repository}")
     public void searchForRepository(String repository) {
         $(".header-search-input").setValue(repository).submit();
     }
+
     @Step("Перейти в репозиторий {repository}")
     public void goToRepository(String repository) {
         $(linkText(repository)).click();
     }
+
     @Step("Перейти в раздел Issues")
     public void openIssuesTab() {
         $(partialLinkText("Issues")).click();
